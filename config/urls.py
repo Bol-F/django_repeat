28b51c1dd4aacf468664c889964b697855cd1876
Bol <bolievfirdavs0@gmail.com>
2026-06-08 01:46:17 +0500

    formated

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    #apps
+    # apps
     path("api/v1/accounts/", include("accounts.urls")),
-
     # jwt
     path("api/v1/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/v1/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
